8c4d487b98d26139cc5cbdde800dcbc37e12e94e
kowalski100 <ijaz.a100@yahoo.com>
2018-04-20 11:26:08 -0700

    printf itm code update

--- Printf-to-Debugger/main.c
+++ Printf-to-Debugger/main.c
@@ -2,11 +2,11 @@
 @author:    Ijaz Ahmad
 
 @descp:     This Program redirects printf string to
-			STLINK-v2 (via ARM ITM cell). i.e. printf
-			messages will can be received from same
-			usb cable connected to STM32f4-Discovery
-			board that programms the onboard micro-
-			controller- no extra hardware is required.
+            STLINK-v2 (via ARM ITM cell). i.e. printf
+            messages will can be received from same
+            usb cable connected to STM32f4-Discovery
+            board that programms the onboard micro-
+            controller- no extra hardware is required.
 
 @warrenty:  void
 */
@@ -16,12 +16,24 @@
 */
 
 #include <stdio.h>
+#include <stdint.h>
+
+extern void SystemCoreClockUpdate(void);
+
+extern uint32_t SystemCoreClock;
+
 
 int main () {
+
 	volatile int i = 0;
-	
+
+  /*
+    once the SystemCoreClockUpdate gets executed. 
+    Examine SystemCoreClock in watch window.
+  */  
+  SystemCoreClockUpdate();
+
 	while (1) {
-		
 		/*
 			This string will be redirected to STLINK on STM32f4-Discovery
 		*/
